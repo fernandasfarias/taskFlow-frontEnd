3635c16361a69ef740d03b0b6a5521f157df929e
fernandasfarias <fernandafariass734@gmail.com>
2026-06-06 22:26:12 -0300

    feat: adiciona rota para tela de perfil

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,25 @@ import Certificacoes from "./pages/onboarding/certificacoes/Certificacoes";
 import Empresa from "./pages/onboarding/empresa/Empresa";
 import Especialidades from "./pages/onboarding/especialidades/Especialidades";
 
+import Profile from "./pages/Perfil/Perfil";
+
 export default function App() {
   return (
     <Routes>
 
+      {/* tela de login e tela de cadastro */}
       <Route path="/" element={<Autenticacao />} />
-      <Route path="/dashboard" element={<DashBoardPage />} />
 
-      {/* onboarding */}
+      {/* onboarding: cadastro das certificacoes, especialidades ou empresa do cliente*/}
       <Route path="/onboarding/certificacoes" element={<Certificacoes />}></Route>
       <Route path="/onboarding/especialidades" element={<Especialidades />}></Route>
       <Route path="/onboarding/empresa" element={<Empresa />}></Route>
 
+      {/* dashboard: tela principal */}
+      <Route path="/dashboard" element={<DashBoardPage />} />
+
+      {/* tela de perfil */}
+      <Route path="/profile" element={<Profile />}></Route>
 
     </Routes>
   );
---
+++ src/pages/Perfil/Perfil.jsx
@@ -0,0 +1,5 @@
+export default function Profile(){
+    return(
+        <div>teste da tela do profile;</div>
+    )
+}
