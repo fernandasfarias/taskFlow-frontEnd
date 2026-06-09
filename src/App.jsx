c7c5ce067fa6b3fe1803bdd229f2a0ece588a61b
luanfelix9 <luan.fdejesus@gmail.com>
2026-06-09 00:02:44 -0300

    feat: foi criada a tela de redefinição de senha, criado o rederecionamento e os metodos so serviço para chamar o back.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Certificacoes from "./pages/onboarding/certificacoes/Certificacoes";
 import Empresa from "./pages/onboarding/empresa/Empresa";
 import Especialidades from "./pages/onboarding/especialidades/Especialidades";
 import RecuperarSenha from "./pages/recuperarSenha/RecuperarSenha";
+import RedefinirSenha from "./pages/redefinirSenha/RedefinirSenha";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <Route path="/" element={<Autenticacao />} />
       <Route path="/dashboard" element={<DashBoardPage />} />
       <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+      <Route path="/redefinir-senha" element={<RedefinirSenha />} />
 
       {/* onboarding */}
       <Route path="/onboarding/certificacoes" element={<Certificacoes />}></Route>
