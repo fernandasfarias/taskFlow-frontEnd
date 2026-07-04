99463b68c39ee3b4ef79b727fc289660b81c28e0
fernandasfarias <fernandafariass734@gmail.com>
2026-07-04 13:13:26 -0300

    fix: corrige as APIs das URLs

--- src/features/dashboard/services/DashboardService.js
+++ src/features/dashboard/services/DashboardService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/dashboard",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/dashboard`,
   headers: {
     "Content-Type": "application/json",
   },
--- src/pages/onboarding/certificacoes/Certificacoes.tsx
+++ src/pages/onboarding/certificacoes/Certificacoes.tsx
@@ -27,7 +27,7 @@ export default function CadastroCertificacoes() {
         urlComprovante: dados.urlComprovante
       }];
 
-      const response = await fetch("http://localhost:8080/onboarding/certificacoes", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/onboarding/certificacoes`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
